3ecfab1aa45d32191935b64fd3a33ca19ebce5dd
casmelad <casmelad@gmail.com>
2017-09-06 18:10:49 -0500

    Binder para soportar punto decimal al envíar datos al server

---
+++ Anxilaris.Utils/Anxilaris.Utils/Web/DoubleModelBinder.cs
@@ -0,0 +1,30 @@
+﻿namespace Anxilaris.Utils.Web.Mvc
+{   
+    using System.Globalization;
+    using System.Web.Mvc;
+    public class DoubleModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var result = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (result != null && !string.IsNullOrEmpty(result.AttemptedValue))
+            {
+                if (bindingContext.ModelType == typeof(double))
+                {
+                    double resultingValue;
+                    var newFormattedValue = result.AttemptedValue.Replace(",", ".");
+                    if (double.TryParse(
+                        newFormattedValue,
+                        NumberStyles.Number,
+                        CultureInfo.InvariantCulture,
+                        out resultingValue)
+                    )
+                    {
+                        return resultingValue;
+                    }
+                }
+            }
+            return base.BindModel(controllerContext, bindingContext);
+        }
+    }
+}
